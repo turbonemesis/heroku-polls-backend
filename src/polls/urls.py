51d1a92512bb298b60d7db4c8d5d195415ae93ea
chase2981 <chase@rentdynamics.com>
2020-03-01 11:21:01 -0700

    added createTask endpoint

--- src/polls/urls.py
+++ src/polls/urls.py
@@ -1,13 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
+from django.conf.urls import include, url
 
+from .twilio.views import TwilioHandler
 from . import views
 
 __app_name__ = 'polls'
 
 router = SimpleRouter()
 router.register('polls', views.QuestionViewSet)
+# router.register('createTask', TwilioHandler.as_view())
 
 urlpatterns = [
+    url(r'^createTask$', TwilioHandler.as_view()),
     path('v1/', include(router.urls)),
 ]
